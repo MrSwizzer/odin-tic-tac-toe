df244680ce2522f938650a8337b315a783275f51
MrSwizzer <155651796+MrSwizzer@users.noreply.github.com>
2024-06-05 12:06:34 +0200

    Add check winner function

--- scripts/script.js
+++ scripts/script.js
@@ -10,17 +10,57 @@ const Gameboard = (function () {
         board[i] = [];
 
         for (let j = 0; j < COLUMNS; j++) {
-            board[i][j] = counter;
-            counter++;
+            board[i][j] = "";
+
         }
     }
     const getBoard = () => board;
 
+    console.log(board);
+
     const setCell = function (row, col, player) {
         board[row][col] = player.getSymbol();
+        checkWinner();
     }
 
-    return { getBoard, setCell }
+    function checkWinner() {
+
+        if (board[0][0] === "X" && board[0][1] === "X" && board[0][2] === "X") {
+            console.log("X WIN'S");
+        } else if (board[1][0] === "X" && board[1][1] === "X" && board[1][2] === "X") {
+            console.log("X WIN'S");
+        } else if (board[2][0] === "X" && board[2][1] === "X" && board[2][2] === "X") {
+            console.log("X WIN'S");
+        }  else if (board[0][0] === "X" && board[1][0] === "X" && board[2][0] === "X") {
+            console.log("X WIN'S");
+        } else if (board[0][1] === "X" && board[1][1] === "X" && board[2][1] === "X") {
+            console.log("X WIN'S");
+        } else if (board[0][2] === "X" && board[1][2] === "X" && board[2][2] === "X") {
+            console.log("X WIN'S");
+        } else if (board[0][0] === "X" && board[1][1] === "X" && board[2][2] === "X") {
+            console.log("X WIN'S");
+        } else if (board[2][0] === "X" && board[1][1] === "X" && board[0][2] === "X") {
+            console.log("X WIN'S");
+        } else if (board[0][0] === "O" && board[0][1] === "O" && board[0][2] === "O") {
+            console.log("O WIN'S");
+        } else if (board[1][0] === "O" && board[1][1] === "O" && board[1][2] === "O") {
+            console.log("O WIN'S");
+        } else if (board[2][0] === "O" && board[2][1] === "O" && board[2][2] === "O") {
+            console.log("O WIN'S");
+        }  else if (board[0][0] === "O" && board[1][0] === "O" && board[2][0] === "O") {
+            console.log("O WIN'S");
+        } else if (board[0][1] === "O" && board[1][1] === "O" && board[2][1] === "O") {
+            console.log("O WIN'S");
+        } else if (board[0][2] === "O" && board[1][2] === "O" && board[2][2] === "O") {
+            console.log("O WIN'S");
+        } else if (board[0][0] === "O" && board[1][1] === "O" && board[2][2] === "O") {
+            console.log("O WIN'S");
+        } else if (board[2][0] === "O" && board[1][1] === "O" && board[0][2] === "O") {
+            console.log("O WIN'S");
+        }
+    }
+
+    return { getBoard, setCell , checkWinner}
 })();
 
 console.log(Gameboard.getBoard());
@@ -34,19 +74,10 @@ function createPlayer(name, symbol) {
     return { getName, getWins, addWin, getSymbol }
 }
 
-/* const player1 = createPlayer("Dani", "X");
+/* const playerX = createPlayer("X Player", "X");
 player1.addWin();
 console.log({
     name : player1.getName(), 
     wins: player1.getWins(),
     symbol: player1.getSymbol()
-});
-
-const player2 = createPlayer("Luk");
-player2.addWin();
-player2.addWin();
-console.log({
-    name : player2.getName(), 
-    wins: player2.getWins()
-}); */
-
+});*/
