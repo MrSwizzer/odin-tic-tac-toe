b6cf514832b9ec856ea9a0a2b8f801ed79d30e91
MrSwizzer <155651796+MrSwizzer@users.noreply.github.com>
2024-06-05 10:07:40 +0200

    Add a factory to create players

--- scripts/script.js
+++ scripts/script.js
@@ -19,6 +19,37 @@ const Gameboard = (function () {
     return {getBoard}
 })();
 
-
 console.log(Gameboard.getBoard());
 
+/* function createPlayer(name) {
+    let wins = 0;
+    const getWins = () => wins;
+    const addWin = () => wins++;
+    const getName = () => name;
+    return {getName, getWins, addWin}
+} */
+
+const playerCreator = (function (name) {
+    let wins = 0;
+    const getWins = () => wins;
+    const addWin = () => wins++;
+    const getName = () => name;
+    return {getName, getWins, addWin}
+})();
+
+playerCreator("Dani").getName();
+
+/* const player1 = createPlayer("Dani");
+player1.addWin();
+console.log({
+    name : player1.getName(), 
+    wins: player1.getWins()
+});
+
+const player2 = createPlayer("Luk");
+player2.addWin();
+player2.addWin();
+console.log({
+    name : player2.getName(), 
+    wins: player2.getWins()
+}); */
